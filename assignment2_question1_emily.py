f328c2ecc9a7d9776bae93722411dd81da6cc398
emilydube <dubee01@nyu.edu>
2017-07-30 19:43:57 -0400

    edits 1

--- assignment2_question1_emily.py
+++ assignment2_question1_emily.py
@@ -1,17 +1,23 @@
 import csv
 from datetime import datetime
 
-def list_to_dict(list):
-    return {'date': list[1], 'taxon': list[22]}
 
+def list_to_dict(list):
+    for item in list:
+        item[1] = (datetime.strptime(item[1], "%Y:%m:%d:%H:%M:%S"))
+    return {'date': item[1], 'taxon': item[22]}
+
+li_of_dicts = []
 with open('cleaned_GPMDB_table.tsv', 'r') as f:
     header = f.readline()
-    li_of_dicts = []
     for line in f:
-        li_lines = [line.strip().split('\t')]
-        for date in li_lines:
-            date[1] = datetime.strptime(date[1], "%Y:%m:%d:%H:%M:%S")
-            for item in li_lines:
-                d = list(map(list_to_dict, li_lines))
-                li_of_dicts.append(d)
-                print(li_of_dicts)
+        li_of_dicts.append([line.strip().split('\t')])
+    mapped_list = (map(list_to_dict, li_of_dicts))
+    filter_list = (filter(lambda d: d[(datetime.strptime("2010/09/30", "%Y/%m/%d")) >= d >= (datetime.strptime("2010/06/01", "%Y/%m/%d"))] in mapped_list))
+    print(list(mapped_list))
+
+
+
+
+
+
