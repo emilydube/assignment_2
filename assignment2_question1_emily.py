927cb07d895f20e53c3b9093bdaf0a079c44c6ee
emilydube <dubee01@nyu.edu>
2017-07-31 23:17:43 -0400

    edits 7.31

--- assignment2_question1_emily.py
+++ assignment2_question1_emily.py
@@ -1,23 +1,23 @@
 import csv
-from datetime import datetime
+from datetime import datetime, date
 
 
 def list_to_dict(list):
-    for item in list:
-        item[1] = (datetime.strptime(item[1], "%Y:%m:%d:%H:%M:%S"))
-    return {'date': item[1], 'taxon': item[22]}
+        return {'date': datetime.strptime(list[1], "%Y:%m:%d:%H:%M:%S"), 'taxon': list[22]}
 
 li_of_dicts = []
 with open('cleaned_GPMDB_table.tsv', 'r') as f:
     header = f.readline()
     for line in f:
-        li_of_dicts.append([line.strip().split('\t')])
-    mapped_list = (map(list_to_dict, li_of_dicts))
-    filter_list = (filter(lambda d: d[(datetime.strptime("2010/09/30", "%Y/%m/%d")) >= d >= (datetime.strptime("2010/06/01", "%Y/%m/%d"))] in mapped_list))
-    print(list(mapped_list))
+        li_of_dicts.append(line.strip().split('\t'))
+    mapped_list = map(list_to_dict, li_of_dicts)
+    start_date = date(2010, 0o6, 0o1)
+    end_date = date(2010, 9, 0o1)
+    filter_list = filter(lambda x: x(start_date, end_date,), mapped_list)
+    print(list(filter_list))
 
 
-
+#  filter_list = filter(lambda x: x in (start_date-end_date), mapped_list)
 
 
 
