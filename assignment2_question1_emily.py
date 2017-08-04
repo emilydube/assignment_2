4c4dadb02e868733a98a1d3f0a1ab7905e430ef7
emilydube <dubee01@nyu.edu>
2017-08-03 21:31:49 -0400

    FINAL

--- assignment2_question1_emily.py
+++ assignment2_question1_emily.py
@@ -2,33 +2,26 @@ from datetime import datetime, date
 from functools import reduce as reduce
 
 
-def func_list_to_dict(list):    #Step 3: function to convert list to dictionary with date and taxon
-    return {'date': datetime.strptime( list[1], "%Y:%m:%d:%H:%M:%S" ), 'taxon': list[22]}
-
-def reduce_counts(f_dicts, dict_of_counts):        #STEP4: reduce function COUNTS for taxon attempt
-    dict_of_counts = {}
-    for x in f_dicts:
-        dict_of_counts[x] = dict_of_counts.get(x, 0) + 1     #wasn't working because you can't (.get) on filter object or list
-        return dict_of_counts
-#
-# def x(a, b):
-#     counts = {}
-#     for x in a:
-#         if x in b:
-#             x[counts] = sum(1 for x in a if x == b)
-#             return counts
-
-# sum( 1 for d in my_list if d.get( 'id' ) == 1 )
+def func_list_to_dict(list_x):  # Step 3: function to convert list to dictionary with date and taxon
+    return {'date': datetime.strptime(list_x[1], "%Y:%m:%d:%H:%M:%S" ), 'taxon': list_x[22]}
 
 
-dict_of_counts = {}
+def reduce_counts(dict_of_counts, f_dicts):     # Step 4: reduce function: counts for taxon
+    taxon_list = f_dicts['taxon'].split(',')    # split the taxon by ,
+    for each_taxon in taxon_list:               # for each taxon in list of dicts, get taxon and count for each
+        each_taxon = each_taxon.strip()
+        dict_of_counts[each_taxon] = dict_of_counts.get(each_taxon, 0) + 1
+    return dict_of_counts
+
+
 li_of_dicts = []
-with open( 'cleaned_GPMDB_table.tsv', 'r' ) as f:
-    header = f.readline()                       #header = so header/ top line is stored
+with open('cleaned_GPMDB_table.tsv', 'r') as f:                               #with open file so it automatically closes
+    header = f.readline()                                                       #header = so header/ top line is stored
     for line in f:
-        li_of_dicts.append( line.strip().split( '\t' ) )
-    mapped_dicts = map( func_list_to_dict, li_of_dicts )    #map to only have date and taxon
-    start_date = datetime( 2010, 6, 1 )
-    end_date = datetime( 2010, 9, 30 )
-    filter_dicts = filter(lambda x: start_date <= x['date'] <= end_date, mapped_dicts) #filtering so only date/ taxons are for stated dates are aggregated
-    reduce(reduce_counts(filter_dicts, dict_of_counts), filter_dicts, {})
+        li_of_dicts.append(line.strip().split('\t'))                        #append each line to list of dicts
+mapped_dicts = map(func_list_to_dict, li_of_dicts)                        #map to only have date and taxon
+start_date = datetime(2010, 6, 1)
+end_date = datetime(2010, 9, 30)
+fil_dt_dicts = filter(lambda x: start_date <= x['date'] <= end_date, mapped_dicts)  #filter for date range/taxons
+reduced_fd = reduce(reduce_counts, fil_dt_dicts, {})
+print(max(reduced_fd.items(), key=lambda x: x[1]))                           #print the taxon with the highest value
